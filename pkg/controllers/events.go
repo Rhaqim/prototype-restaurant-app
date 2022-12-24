7616fd5492d37af368f9fcdb202ace798c241907
Rhaqim <anusiemj@gmail.com>
2022-12-24 17:44:51 +0100

    working on sending requests for events to friends

--- pkg/controllers/events.go
+++ pkg/controllers/events.go
@@ -19,6 +19,7 @@ import (
 var eventCollection = config.EventCollection
 var orderCollection = config.OrderCollection
 var productCollection = config.ProductCollection
+var friendshipCollection = config.FriendshipCollection
 
 func CreateEvent(c *gin.Context) {
 	ctx, cancel := context.WithTimeout(c.Request.Context(), 10*time.Second)
@@ -53,6 +54,7 @@ func CreateEvent(c *gin.Context) {
 		"title":     request.Title,
 		"hostId":    user.ID,
 		"hostedIds": request.HostedIDs,
+		"orders":    request.Orders,
 		"venue":     request.Venue,
 		"type":      request.Type,
 		"bill":      request.Bill,
@@ -304,5 +306,124 @@ func CreateOrder(c *gin.Context) {
 	wg.Wait()
 
 	response := hp.SetSuccess("Order created", insertResult, funcName)
+	c.JSON(http.StatusOK, response)
+}
+
+func GetOrders(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(c.Request.Context(), 10*time.Second)
+	defer cancel()
+	defer database.ConnectMongoDB().Disconnect(context.TODO())
+
+	var funcName = ut.GetFunctionName()
+
+	user, err := hp.GetUserFromToken(c)
+	if err != nil {
+		response := hp.SetError(err, "User not logged in", funcName)
+		c.AbortWithStatusJSON(http.StatusBadRequest, response)
+		return
+	}
+
+	filter := bson.M{"customer.id": user.ID}
+
+	cursor, err := orderCollection.Find(ctx, filter)
+	if err != nil {
+		response := hp.SetError(err, "Error getting orders", funcName)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, response)
+		return
+	}
+
+	var orders []hp.Order
+	if err = cursor.All(ctx, &orders); err != nil {
+		response := hp.SetError(err, "Error getting orders", funcName)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, response)
+		return
+	}
+
+	response := hp.SetSuccess("Orders retrieved", orders, funcName)
+	c.JSON(http.StatusOK, response)
+}
+
+func GetOrder(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(c.Request.Context(), 10*time.Second)
+	defer cancel()
+	defer database.ConnectMongoDB().Disconnect(context.TODO())
+
+	var funcName = ut.GetFunctionName()
+
+	user, err := hp.GetUserFromToken(c)
+	if err != nil {
+		response := hp.SetError(err, "User not logged in", funcName)
+		c.AbortWithStatusJSON(http.StatusBadRequest, response)
+		return
+	}
+
+	order_id, err := primitive.ObjectIDFromHex(c.Param("id"))
+	if err != nil {
+		response := hp.SetError(err, "Error converting id to object id", funcName)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, response)
+		return
+	}
+
+	filter := bson.M{"_id": order_id, "customer.id": user.ID}
+
+	var order hp.Order
+	err = orderCollection.FindOne(ctx, filter).Decode(&order)
+	if err != nil {
+		response := hp.SetError(err, "Error getting order", funcName)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, response)
+		return
+	}
+
+	response := hp.SetSuccess("Order retrieved", order, funcName)
+	c.JSON(http.StatusOK, response)
+}
+
+func InviteFriendToEvent(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(c.Request.Context(), 10*time.Second)
+	defer cancel()
+	defer database.ConnectMongoDB().Disconnect(context.TODO())
+
+	var funcName = ut.GetFunctionName()
+
+	user, err := hp.GetUserFromToken(c)
+	if err != nil {
+		response := hp.SetError(err, "User not logged in", funcName)
+		c.AbortWithStatusJSON(http.StatusBadRequest, response)
+		return
+	}
+
+	var request hp.InviteFriendToEventRequest
+	err = c.ShouldBindJSON(&request)
+	if err != nil {
+		response := hp.SetError(err, "Error binding request", funcName)
+		c.AbortWithStatusJSON(http.StatusBadRequest, response)
+		return
+	}
+
+	// check if event exists
+	event_id, err := primitive.ObjectIDFromHex(request.Event.ID.Hex())
+	if err != nil {
+		response := hp.SetError(err, "Error converting id to object id", funcName)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, response)
+		return
+	}
+
+	filter := bson.M{"_id": event_id}
+	var event hp.Event
+	err = eventCollection.FindOne(ctx, filter).Decode(&event)
+	if err != nil {
+		response := hp.SetError(err, "Error getting event", funcName)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, response)
+		return
+	}
+
+	// check if friendship exists
+	if !hp.VerifyFriends(user, request.Friend.ID) {
+		response := hp.SetError(err, "Friendship does not exist", funcName)
+		c.AbortWithStatusJSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := hp.SetSuccess("Friend invited", request.Friend.Username, funcName)
 	c.JSON(http.StatusOK, response)
 }
--- pkg/helpers/event.go
+++ pkg/helpers/event.go
@@ -22,13 +22,14 @@ func (h EventType) String() string {
 
 type EventCreate struct {
 	Title     string               `json:"title" binding:"required"`
-	HostedIDs []primitive.ObjectID `json:"hostedIds" binding:"required" bson:"hostedIds"`
-	Orders    Orders               `json:"orders" bson:"orders"`
+	HostedIDs []primitive.ObjectID `json:"hosted_ids" binding:"required" bson:"hosted_ids" default:"[]"`
+	Orders    Orders               `json:"orders" bson:"orders" default:"[]"`
 	Venue     primitive.ObjectID   `json:"venue" bson:"venue"`
 	Type      EventType            `json:"type" bson:"type" default:"close"`
-	Bill      int                  `json:"bill" bson:"bill" binding:"required,number" default:"0"`
-	CreatedAt primitive.DateTime   `bson:"createdAt" json:"createdAt" default:"Now()"`
-	UpdatedAt primitive.DateTime   `bson:"updatedAt" json:"updatedAt" default:"Now()"`
+	Budget    float64              `json:"budget" bson:"budget" binding:"number" default:"0"`
+	Bill      float64              `json:"bill" bson:"bill" binding:"number" default:"0"`
+	CreatedAt primitive.DateTime   `bson:"created_at" json:"created_at" default:"Now()"`
+	UpdatedAt primitive.DateTime   `bson:"updated_at" json:"updated_at" default:"Now()"`
 }
 
 type Event struct {
@@ -36,9 +37,65 @@ type Event struct {
 	Title     string               `json:"title" binding:"required" bson:"title"`
 	HostID    primitive.ObjectID   `json:"hostId" bson:"hostId"`
 	HostedIDs []primitive.ObjectID `json:"hostedIds" bson:"hostedIds"`
+	Orders    Orders               `json:"orders" bson:"orders" default:"[]"`
 	Venue     primitive.ObjectID   `json:"venue" bson:"venue"`
 	Type      EventType            `json:"type" bson:"type"`
-	Bill      int                  `json:"bill" bson:"bill"`
-	CreatedAt primitive.DateTime   `bson:"createdAt" json:"createdAt" omitEmpty:"true"`
-	UpdatedAt primitive.DateTime   `bson:"updatedAt" json:"updatedAt" default:"Now()"`
+	Budget    float64              `json:"budget" bson:"budget" binding:"number" default:"0"`
+	Bill      float64              `json:"bill" bson:"bill"`
+	CreatedAt primitive.DateTime   `bson:"created_at" json:"created_at" omitEmpty:"true"`
+	UpdatedAt primitive.DateTime   `bson:"updated_at" json:"updated_at" default:"Now()"`
+}
+
+type InviteFriendToEventRequest struct {
+	Event  Event      `json:"event_id" bson:"event_id"`
+	Friend UserStruct `json:"user_id" bson:"user_id"`
+}
+
+type AttendingStatus string
+
+const (
+	Invited      AttendingStatus = "invited"
+	Attending    AttendingStatus = "attending"
+	NotAttending AttendingStatus = "not attending"
+)
+
+func (h AttendingStatus) String() string {
+	switch h {
+	case Invited:
+		return "invited"
+	case Attending:
+		return "attending"
+	case NotAttending:
+		return "not attending"
+	default:
+		return "invited"
+	}
+}
+
+type EventAttendee struct {
+	EventID    primitive.ObjectID `json:"event_id" bson:"event_id"`
+	UserID     primitive.ObjectID `json:"user_id" bson:"user_id"`
+	Status     AttendingStatus    `json:"status" bson:"status"`
+	InvitedBy  primitive.ObjectID `json:"invited_by" bson:"invited_by"`
+	InvitedAt  primitive.DateTime `json:"invited_at" bson:"invited_at"`
+	AttendedAt primitive.DateTime `json:"attended_at" bson:"attended_at"`
+}
+
+func SendInviteToEvent(event Event, friend UserStruct) {
+	// Send invite to friend
+}
+
+func InviteFriendToEvent(event Event, friend UserStruct) {
+	// Create invite
+	// Send invite to friend
+}
+
+func AcceptInviteToEvent(event Event, friend UserStruct) {
+	// Update invite
+	// Send invite to friend
+}
+
+func DeclineInviteToEvent(event Event, friend UserStruct) {
+	// Update invite
+	// Send invite to friend
 }
