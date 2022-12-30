e85b57d3b26f318857aba166fb2708dad3e4b329
Rhaqim <anusiemj@gmail.com>
2022-12-30 01:19:40 +0100

    created fund wallet endpoint

---
+++ pkg/controllers/wallets.go
@@ -0,0 +1,51 @@
+package controllers
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/Rhaqim/thedutchapp/pkg/database"
+	hp "github.com/Rhaqim/thedutchapp/pkg/helpers"
+	ut "github.com/Rhaqim/thedutchapp/pkg/utils"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func FundWallet(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(c.Request.Context(), 10*time.Second)
+	defer cancel()
+	defer database.ConnectMongoDB().Disconnect(context.TODO())
+
+	var funcName = ut.GetFunctionName()
+
+	request := hp.FundWalletRequest{}
+
+	if err := c.ShouldBindJSON(&request); err != nil {
+		response := hp.SetError(err, "Error binding json", funcName)
+		c.AbortWithStatusJSON(http.StatusBadRequest, response)
+		return
+	}
+
+	user, err := hp.GetUserFromToken(c)
+	if err != nil {
+		response := hp.SetError(err, "User not logged in", funcName)
+		c.AbortWithStatusJSON(http.StatusBadRequest, response)
+		return
+	}
+
+	user.Wallet += request.Amount
+
+	filter := bson.M{"_id": user.ID}
+	update := bson.M{"$set": bson.M{"wallet": user.Wallet}}
+
+	_, err = authCollection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		response := hp.SetError(err, "Error updating user", funcName)
+		c.AbortWithStatusJSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := hp.SetSuccess("Wallet funded", user, funcName)
+	c.JSON(http.StatusOK, response)
+}
---
+++ pkg/helpers/wallet.go
@@ -0,0 +1,5 @@
+package helpers
+
+type FundWalletRequest struct {
+	Amount float64 `json:"amount" bson:"amount"`
+}
